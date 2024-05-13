7b9c69e753f1081d139de53df0b98544f084079f
NicoMB2002 <nicomoncayo@hotmail.com>
2024-05-13 14:09:53 -0400

    Added stuff to main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,30 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Scanner console = new Scanner(System.in);
+
+        System.out.println("Please enter a distance in km: ");
+
+        double distance = console.nextDouble();
+
+        double mileDistance = distance / 1.60934;
+
+        double gallonAmount =  mileDistance / 11;
+
+        double gasLitre = gallonAmount * 3.78541;
+
+        double gasPrice = gasLitre * 1.33;
+
+        System.out.printf("Distance(km): %.2f\n" +
+                        "Distance(miles); %.2f\n" +
+                        "Gas(gallon); %.2f\n" +
+                        "Gas(litre): %.2f\n" +
+                        "Price: %.2f$",
+                distance, mileDistance, gallonAmount, gasLitre, gasPrice);
     }
 }
