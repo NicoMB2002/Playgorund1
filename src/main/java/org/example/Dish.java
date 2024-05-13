b4807ec144d5265495846e4bb21ed8feb4db7759
NicoMB2002 <nicomoncayo@hotmail.com>
2024-05-13 14:24:50 -0400

    created Dish class

--- src/main/java/org/example/Dish.java
+++ src/main/java/org/example/Dish.java
@@ -1,7 +1,35 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Dish {
 
     private String name;
     private int price;
+
+    public Dish(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Dish{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dish dish = (Dish) o;
+        return price == dish.price && Objects.equals(name, dish.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
 }
