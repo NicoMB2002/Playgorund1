ca959c45f01f508b8ba407374092789352f77056
NicoMB2002 <nicomoncayo@hotmail.com>
2024-05-13 14:34:07 -0400

    Created Tea class

---
+++ src/main/java/org/example/Tea.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Tea {
+    private String name;
+    private int num;
+
+    public Tea(String name, int num) {
+        this.name = name;
+        this.num = num;
+    }
+}
